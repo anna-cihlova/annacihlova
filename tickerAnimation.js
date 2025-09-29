663a49630b4d9ee27387f70556baada3fefae82f
Cihlička <33567545+anna-cihlova@users.noreply.github.com>
2025-09-28 23:07:14 -0500

    Updated ticket animation functionality

--- tickerAnimation.js
+++ tickerAnimation.js
@@ -1,61 +1,26 @@
-var $tickerWrapper = $(".tickerwrapper");
-var $list = $tickerWrapper.find("ul.list");
-var $clonedList = $list.clone();
-var listWidth = 10;
+const tickerWrapper = document.querySelector(".tickerwrapper");
+const list = tickerWrapper.querySelector("ul.list");
+const clonedList = list.cloneNode(true);
 
-$list.find("li").each(function (i) {
-  listWidth += $(this, i).outerWidth(true);
+let listWidth = 0;
+list.querySelectorAll("li").forEach((li) => {
+  listWidth += li.offsetWidth;
 });
 
-var endPos = $tickerWrapper.width() - listWidth;
+list.style.width = `${listWidth}px`;
+clonedList.classList.add("cloned");
+clonedList.style.width = `${listWidth}px`;
+tickerWrapper.appendChild(clonedList);
 
-$list.add($clonedList).css({
-  width: listWidth + "px",
-});
+const tl = gsap.timeline({ repeat: -1, defaults: { ease: "none" } });
+const duration = 40;
 
-$clonedList.addClass("cloned").appendTo($tickerWrapper);
+tl.fromTo(list, { x: 0 }, { x: -listWidth, duration }).fromTo(
+  clonedList,
+  { x: listWidth },
+  { x: 0, duration },
+  0
+);
 
-//TimelineMax
-var infinite = new TimelineMax({ repeat: -1, paused: true });
-var time = 40;
-
-infinite
-  .fromTo(
-    $list,
-    time,
-    { rotation: 0.01, x: 0 },
-    { force3D: true, x: -listWidth, ease: Linear.easeNone },
-    0
-  )
-  .fromTo(
-    $clonedList,
-    time,
-    { rotation: 0.01, x: listWidth },
-    { force3D: true, x: 0, ease: Linear.easeNone },
-    0
-  )
-  .set($list, { force3D: true, rotation: 0.01, x: listWidth })
-  .to(
-    $clonedList,
-    time,
-    { force3D: true, rotation: 0.01, x: -listWidth, ease: Linear.easeNone },
-    time
-  )
-  .to(
-    $list,
-    time,
-    { force3D: true, rotation: 0.01, x: 0, ease: Linear.easeNone },
-    time
-  )
-  .progress(1)
-  .progress(0)
-  .play();
-
-//Pause/Play
-$tickerWrapper
-  .on("mouseenter", function () {
-    infinite.pause();
-  })
-  .on("mouseleave", function () {
-    infinite.play();
-  });
+tickerWrapper.addEventListener("mouseenter", () => tl.pause());
+tickerWrapper.addEventListener("mouseleave", () => tl.play());
