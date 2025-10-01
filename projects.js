5afff2fe83acde23d35c669b26d9be630bccfcf2
Cihlička <33567545+anna-cihlova@users.noreply.github.com>
2025-10-01 13:51:09 -0500

    Added more projects

--- projects.js
+++ projects.js
@@ -13,7 +13,7 @@ export const projects = [
     title: "Blockchain",
     tools: ["JavaScript", "HTML", "CSS/SCSS"],
     description:
-      "Coded a website for TopMonks Blockchain, a development studio focused on blockchain technologies. Built with HTML, CSS/SCSS, and JavaScript, featuring a responsive layout and modern UI elements.",
+      "Coded a website for TopMonks Blockchain, a development studio focused on blockchain technologies. Built with a responsive layout and modern UI elements.",
     image: "blockchain.webp",
     alt: "Landing page for TopMonks Blockchain",
     href: "https://blockchain.topmonks.com/",
@@ -30,10 +30,30 @@ export const projects = [
     in_production: true,
   },
   {
+    title: "Security Lab",
+    tools: ["JavaScript", "HTML", "CSS/SCSS"],
+    description:
+      "Developed the Security Lab landing page for TopMonks, a showcase of expertise in cybersecurity. The site listed the areas they provide support in, including hardware, software, and social engineering, presented with clear navigation and engaging content.",
+    image: "securitylab.webp",
+    alt: "Landing page for Security Lab",
+    href: "https://securitylab.topmonks.com/",
+    in_production: true,
+  },
+  {
+    title: "OM Platform",
+    tools: ["JavaScript", "HTML", "CSS/SCSS"],
+    description:
+      "Developed a responsive landing page for OM Platform, an online investment and share management tool for Czech businesses. Implemented a custom form and interactive animations to guide users through the platform’s features.",
+    image: "omplatform.webp",
+    alt: "Landing page for OM Platform",
+    href: "https://www.omplatform.cz/",
+    in_production: true,
+  },
+  {
     title: "Coding for Kids",
     tools: ["JavaScript", "HTML", "CSS/SCSS"],
     description:
-      "Created an interactive website to spark children’s interest in programming, technology, and robotics. The site was playful, colorful, and engaging, offering classes for kids of all ages. Built with HTML, CSS/SCSS, and JavaScript.",
+      "Created an interactive website to spark children’s interest in programming, technology, and robotics. The site was playful, colorful, and engaging, offering classes for kids of all ages.",
     image: "for-kids.webp",
     alt: "Landing page for Coding for Kids",
     href: "https://prodeti.topmonks.cz/",
@@ -43,7 +63,7 @@ export const projects = [
     title: "TopMonks",
     tools: ["JavaScript", "HTML", "CSS/SCSS"],
     description:
-      "Developed the company’s bilingual landing page (Czech & English) with a responsive, mobile-friendly design. Implemented features like a sticky navigation bar, animated buttons, and smooth background transitions using HTML, CSS/SCSS, and JavaScript.",
+      "Developed the company’s bilingual landing page (Czech & English) with a responsive, mobile-friendly design. Implemented features like a sticky navigation bar, animated buttons, and smooth background transitions.",
     image: "topmonks.webp",
     alt: "Landing page for TopMonks",
     in_production: false,
@@ -52,7 +72,7 @@ export const projects = [
     title: "PostCube",
     tools: ["JavaScript", "HTML", "CSS/SCSS"],
     description:
-      "Built a landing page for PostCube, a private mailbox service. Added an animated navigation bar, integrated a video section, and designed a user-friendly contact form using HTML, CSS/SCSS, and JavaScript.",
+      "Built a landing page for PostCube, a private mailbox service. Added an animated navigation bar, integrated a video section, and designed a user-friendly contact form.",
     video: "postcube.mp4",
     alt: "Landing page for PostCube",
     in_production: false,
